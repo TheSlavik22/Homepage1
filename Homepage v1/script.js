7c2c9c81b804d0bad042fcb7dc9a8cf77c75f02b
TheSlavik22 <slavyanka345@mail.ru>
2023-11-02 15:44:24 +1000

    init commit

---
+++ Homepage v1/script.js
@@ -0,0 +1,28 @@
+let currentAudio = null; // переменная для хранения текущего аудио
+
+function playAudio(audioSrc) {
+    return new Promise((resolve) => {
+        // Проверяем, играет ли текущее аудио
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0; // Сбрасываем в начало для возможности повторного воспроизведения
+        }
+
+        // Создаем новое аудио
+        var audio = new Audio(audioSrc);
+        currentAudio = audio; // Устанавливаем текущее аудио
+        audio.play();
+
+        // Добавляем анимацию разворота
+        var image = event.target;
+        image.style.transition = 'transform 0.5s ease-in-out';
+        image.style.transform = 'scale(1.1) rotate(360deg)';
+
+        // Сбрасываем анимацию и возвращаем изображение в исходное состояние через 0.5 секунды
+        setTimeout(function() {
+            image.style.transition = 'none';
+            image.style.transform = 'scale(1) rotate(0deg)';
+            resolve();
+        }, 500);
+    });
+}

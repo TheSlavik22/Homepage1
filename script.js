9ae2107961144e93b5b0cd889c184e2a8021825c
TheSlavik22 <slavyanka345@mail.ru>
2023-11-02 16:04:07 +1000

    updated photo and text in index.html and script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-let currentAudio = null; // переменная для хранения текущего аудио
+let currentAudio = null;
 
 function playAudio(audioSrc) {
     return new Promise((resolve) => {
         if (currentAudio) {
             currentAudio.pause();
-            currentAudio.currentTime = 0; // Сбрасываем в начало для возможности повторного воспроизведения
+            currentAudio.currentTime = 0;
         }
 
-        // Создаем новое аудио
         var audio = new Audio(audioSrc);
-        currentAudio = audio; // Устанавливаем текущее аудио
+        currentAudio = audio;
         audio.play();
 
-        // Добавляем анимацию разворота
         var image = event.target;
         image.style.transition = 'transform 0.5s ease-in-out';
         image.style.transform = 'scale(1.1) rotate(360deg)';
 
-        // Сбрасываем анимацию и возвращаем изображение в исходное состояние через 0.5 секунды
         setTimeout(function() {
             image.style.transition = 'none';
             image.style.transform = 'scale(1) rotate(0deg)';
